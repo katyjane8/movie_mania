45fc54f33154b745cd437c864566c8a88525cc41
katyjane8 <katy.jane8@gmail.com>
2017-12-08 11:01:43 -0700

    new schema with directors tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207204613) do
+ActiveRecord::Schema.define(version: 20171208173555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "directors", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
