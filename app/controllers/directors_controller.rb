d55c78c0132c8d1e4505c6d53c3bb50d83e413f2
katyjane8 <katy.jane8@gmail.com>
2017-12-11 10:10:44 -0700

    editing show method and created private methods

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,6 @@
 class DirectorsController < ApplicationController
+  before_action :set_director, only: [:show]
+
   def index
     @directors = Director.all
   end
@@ -6,4 +8,27 @@ class DirectorsController < ApplicationController
   def new
     @directors = Director.new
   end
+
+  def create
+    director = Director.new(director_params)
+    if director.save
+      redirect_to "/directors/#{director.id}"
+    else
+      render :new
+    end
+  end
+
+  def show
+    redirect_to director_path()
+  end
+
+  private
+
+    def director_params
+      params.require(:director).permit(:name)
+    end
+
+    def set_director
+      @director = Director.find(params[:id])
+    end
 end
