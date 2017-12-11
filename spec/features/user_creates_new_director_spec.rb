738d5b772b8fda371c5a33af20d69afbcefa6a35
katyjane8 <katy.jane8@gmail.com>
2017-12-11 09:25:10 -0700

    created the new test for new directors

---
+++ spec/features/user_creates_new_director_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "As a user, when I visit '/directors/new'" do
+  context "they fill out a form with name" do
+    it "creates a new director and redirects to director show" do
+      visit '/directors/new'
+
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content("Create a New Director")
+
+      fill_in "director[name]", with: "Steven Spielberg"
+
+      click_on "Create Director"
+
+      expect(current_path).to eq("/directors/#{Director.last.id}")
+    end
+  end
+end
