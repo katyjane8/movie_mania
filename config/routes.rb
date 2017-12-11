e99f248307783331fcacdfa12661d10dd88c4799
katyjane8 <katy.jane8@gmail.com>
2017-12-11 09:32:36 -0700

    add resource director to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/movies', to: 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :directors
   resources :movies
 end
