7094736b443cce9091ec0714255e00d4cf848bf1
katyjane8 <katy.jane8@gmail.com>
2017-12-18 11:01:07 -0700

    passing feature test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   # get '/movies', to: 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "welcome#index"
+
   resources :directors, shallow: true do
     resources :movies
   end
-  
+
   resources :movies do
     resources :actors
   end
+
+  resources :users, only: [:show, :new, :create]
+
 end
