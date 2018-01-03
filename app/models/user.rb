807ba2dfb8bc2b905765f2d9906dba69040ea844
katyjane8 <katy.jane8@gmail.com>
2018-01-02 21:02:58 -0700

    adding sessions and admin to movies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
+  enum role: ["default", "admin"]
+
+  validates_uniqueness_of :username
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    username "funbucket13"
+    password "testing"
+    role 0
+  end
+end
